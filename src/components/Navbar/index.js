2d2e92f355a8fccb3778efead398ef8b2d3e9ed5
SaifN97 <saifnarpali@gmail.com>
2021-11-08 17:23:02 +0530

    Added Hero section and mobile menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,12 +12,12 @@ import {
   NavMenu,
 } from "./NavbarElements";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">dolla</NavLogo>
-        <MobileIcon>
+        <MobileIcon onClick={toggle}>
           <FaBars />
         </MobileIcon>
         <NavMenu>
